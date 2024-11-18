18c6aaddd1a6fdfa3a7fb107c9ce42f78e49f7e0
Brian Moore <brianmoore0213@gmail.com>
2024-11-18 14:26:59 -0500

    fixed grid

--- ui/src/components/container/Grid.tsx
+++ ui/src/components/container/Grid.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 export const Grid: React.FC<Props> = ({ children }) => {
   return (
-    <div className="grid gap-4 pt-4 mx-auto w-full max-w-[960px] grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid gap-4 pt-4 mx-auto w-full max-w-[1280px] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
       {children}
     </div>
   );
