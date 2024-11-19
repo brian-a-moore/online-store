d3b0fcdc40b167a4fe051de1398c9602fc2b1072
Brian Moore <brianmoore0213@gmail.com>
2024-11-19 17:20:06 -0500

    schema updates

--- api/src/middlewares/schemaValidator.middleware.ts
+++ api/src/middlewares/schemaValidator.middleware.ts
@@ -1,19 +1,19 @@
 import { STATUS_CODE } from '@sunami/constants';
 import { NextFunction, Request, Response } from 'express';
-import { ZodSchema } from 'zod';
+import { ZodObject } from 'zod';
 import logger from '../config/logger';
 
-export default (schema: { body: ZodSchema<any>; params: ZodSchema<any>; query: ZodSchema<any> }) => {
+export default (schema: { body: ZodObject<any>; params: ZodObject<any>; query: ZodObject<any> }) => {
   return (req: Request, res: Response, next: NextFunction) => {
     try {
       logger.debug('SCHEMA VALIDATOR MIDDLEWARE: Validating schema...');
-      const validatedBody = schema.body.parse(req.body);
-      const validatedParams = schema.params.parse(req.params);
-      const validatedQuery = schema.query.parse(req.query);
-      req.body = validatedBody;
-      req.params = validatedParams;
-      req.query = validatedQuery;
-      logger.debug('SCHEMA VALIDATOR MIDDLEWARE: Schema validated', { params: validatedParams, body: validatedBody });
+
+      req.body = schema.body.parse(req.body);
+      req.params = schema.params.parse(req.params);
+      req.query = schema.query.parse(req.query);
+
+      logger.debug('SCHEMA VALIDATOR MIDDLEWARE: Schema validated');
+
       next();
     } catch (e: any | unknown) {
       logger.error('SCHEMA VALIDATOR MIDDLEWARE: Schema validation failed', {
--- api/src/schemas/_presets.ts
+++ api/src/schemas/_presets.ts
@@ -1,13 +1,9 @@
 import { z } from 'zod';
 
 export const bool = z.boolean();
-
-export const empty = z.object({}).refine((data) => Object.keys(data).length === 0, {
-  message: 'Object must be empty',
-});
-
+export const empty = z.object({}).strict();
 export const itemType = z.enum(['donation', 'merchandise', 'ticket']);
-export const obj = (args: any) => z.object(args);
+export const obj = (args: any) => z.object(args).strict();
 export const price = z.number().positive().min(1).max(999999);
 export const qty = z.number().int().positive().min(1).max(999);
 export const role = z.number().int().min(1).max(3);
