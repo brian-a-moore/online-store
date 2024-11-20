7406956bfd470dbd9136ce1984478b96fd5cdf12
Brian Moore <brianmoore0213@gmail.com>
2024-11-20 12:12:54 -0500

    missed one

--- api/src/controllers/user/get.ts
+++ api/src/controllers/user/get.ts
@@ -1,6 +1,10 @@
 import { STATUS_CODE } from '@sunami/constants';
 import { Request, Response } from 'express';
+import { GetUserBody, GetUserParams, GetUserQuery } from '../../types/routes';
 
-export const getUserController = async (req: Request, res: Response) => {
+export const getUserController = async (
+  req: Request<GetUserParams, unknown, GetUserBody, GetUserQuery>,
+  res: Response,
+) => {
   res.status(STATUS_CODE.NOT_IMPLEMENTED).json({ message: 'getUser' });
 };
