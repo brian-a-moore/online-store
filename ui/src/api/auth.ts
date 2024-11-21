f444a2af681b63e72442ac4b5aed82d950cbd121
Brian Moore <brianmoore0213@gmail.com>
2024-11-21 16:53:13 -0500

    fixed auth api calls

--- ui/src/api/auth.ts
+++ ui/src/api/auth.ts
@@ -10,17 +10,25 @@ import { apiCall } from '../config/axios';
 import { HTTP_METHOD } from '../constants'; // dpr
 
 export const authLogin = async (loginCredentials: any) => {
-  return apiCall<AuthLoginBody, AuthLoginQuery, AuthLoginResponse>({
-    url: `/auth/login`,
-    method: HTTP_METHOD.POST,
-    data: loginCredentials,
-  });
+  return apiCall<AuthLoginBody, AuthLoginQuery, AuthLoginResponse>(
+    {
+      url: `/auth/login`,
+      method: HTTP_METHOD.POST,
+      data: loginCredentials,
+    },
+    undefined,
+    false,
+  );
 };
 
 export const authVerifyToken = async (token: string) => {
-  return apiCall<AuthVerifyTokenBody, AuthVerifyTokenQuery, AuthVerifyTokenResponse>({
-    url: `/auth/verify_token`,
-    method: HTTP_METHOD.POST,
-    data: { token },
-  });
+  return apiCall<AuthVerifyTokenBody, AuthVerifyTokenQuery, AuthVerifyTokenResponse>(
+    {
+      url: `/auth/verify_token`,
+      method: HTTP_METHOD.POST,
+      data: { token },
+    },
+    undefined,
+    false,
+  );
 };
