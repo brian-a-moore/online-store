41d349b63367cba744c80642c85f68db36b2f8d3
Brian Moore <brianmoore0213@gmail.com>
2024-11-21 16:24:43 -0500

    updated cart styles

--- ui/src/components/core/Cart.tsx
+++ ui/src/components/core/Cart.tsx
@@ -4,6 +4,7 @@ import { useContext, useState } from 'react';
 import { CartContext } from '../../context/CartContext';
 import { formatCurrency, getTotalPrice, totalItems } from '../../utils';
 import { Button, Stepper } from '../interactive';
+import { H3, H4, H5 } from '../typography';
 
 type Props = {};
 
@@ -30,50 +31,45 @@ export const Cart: React.FC<Props> = () => {
 
   return (
     <div
-      className={`fixed flex flex-col flex-1 p-0 gap-0 right-4 bottom-4 z-20 overflow-hidden max-h-[60vh] ${isMaximized ? 'w-80' : 'w-auto'} bg-white border-[1px] border-gray-300 rounded shadow-md`}
+      className={`fixed flex flex-col flex-1 p-0 gap-0 right-4 bottom-4 z-20 overflow-hidden max-h-[60vh] ${isMaximized ? 'w-96' : 'w-auto'} bg-white border-[1px] border-gray-300 rounded shadow-md`}
     >
       <div className="flex px-4 py-2 gap-4 border-b-2 bg-gray-100 items-center justify-between">
-        <p className="font-semibold justify-between">Cart ({totalItems(items)})</p>
+        <H4 className="justify-between">Cart ({totalItems(items)})</H4>
         <button onClick={() => setIsMaximized((prevState) => !prevState)}>
           <Icon path={isMaximized ? mdiChevronDown : mdiChevronUp} size={1} />
         </button>
       </div>
       {isMaximized ? (
         <>
-          <div className="flex-1 overflow-y-auto">
+          <div className="flex flex-col flex-1 p-4 gap-4 overflow-y-auto">
             {items.length > 0 ? (
               <>
                 {items.map((item) => (
-                  <div key={item.id} className="flex flex-col gap-2 p-4 items-center">
-                    <h1
-                      className="font-semibold w-full mb-[-8px] whitespace-nowrap overflow-ellipsis overflow-hidden"
-                      title={item.name}
-                    >
-                      {item.name}
-                    </h1>
-                    <h2
-                      className="text-sm w-full whitespace-nowrap overflow-ellipsis overflow-hidden"
+                  <div key={item.id} className="flex flex-col border-b-2 pb-4">
+                    <H3 title={item.name}> {item.name} </H3>
+                    <p
+                      className="text-sm w-full whitespace-nowrap overflow-ellipsis overflow-hidden mb-4"
                       title={item.product.name}
                     >
                       {item.product.name}
-                    </h2>
-                    <div className="flex gap-2">
+                    </p>
+                    <div className="flex gap-2 items-center">
                       <Stepper item={item} handleQuantityChange={handleQuantityChange} />
-                      <p>{formatCurrency(item.quantity * item.price)}</p>
-                      <Button variant="destructive" onClick={() => handleDeleteItem(item.id)} title="Delete Item">
-                        <Icon path={mdiDelete} size={0.55} />
+                      <p className='flex-1 px-2'>{formatCurrency(item.quantity * item.price)}</p>
+                      <Button className='opacity-30 hover:opacity-100' variant="destructive" onClick={() => handleDeleteItem(item.id)} title="Delete Item">
+                        <Icon path={mdiDelete} size={0.75} />
                       </Button>
                     </div>
                   </div>
                 ))}
               </>
             ) : (
-              <p className="text-sm text-center opacity-50 p-4">Your cart is empty</p>
+              <p className="text-sm text-center opacity-50">Your cart is empty</p>
             )}
           </div>
           {items.length ? (
             <div className="flex gap-2 border-t-2 bg-gray-100 items-center justify-between px-4 py-2">
-              <h1 className="font-semibold">TOTAL: {formatCurrency(getTotalPrice(items))}</h1>
+              <H5>TOTAL: {formatCurrency(getTotalPrice(items))}</H5>
               <form action="http://localhost:8080/order/checkout" method="POST">
                 <Button type="submit">Checkout</Button>
               </form>
