bfed3cff85f2b43ccc8145d43796df2383577a00
Brian Moore <brianmoore0213@gmail.com>
2024-11-21 16:23:10 -0500

    fixed api universal export

--- ui/src/api/index.ts
+++ ui/src/api/index.ts
@@ -1,5 +1,7 @@
-export * from './auth';
-export * from './item';
-export * from './product';
-export * from './store';
-export * from './user';
+import * as auth from './auth';
+import * as item from './item';
+import * as product from './product';
+import * as store from './store';
+import * as user from './user';
+
+export const api = { auth, item, product, store, user };
