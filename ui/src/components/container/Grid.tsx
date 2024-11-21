572501a33d313ad11b07d77442603ba3633cd9a1
Brian Moore <brianmoore0213@gmail.com>
2024-11-21 16:31:38 -0500

    added 50% height to grid so that it won't block the cart

--- ui/src/components/container/Grid.tsx
+++ ui/src/components/container/Grid.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 export const Grid: React.FC<Props> = ({ children }) => {
   return (
-    <div className="grid gap-4 pt-4 mx-auto w-full max-w-[1280px] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div className="grid gap-4 p-4 mx-auto w-full max-w-[1280px] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 pb-[50vh]">
       {children}
     </div>
   );
