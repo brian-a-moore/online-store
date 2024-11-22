60eca3dd5d027bd39bf8e72a40f3e58369b0a92e
Brian Moore <brianmoore0213@gmail.com>
2024-11-22 12:53:43 -0500

    icon and banner images

---
+++ ui/src/components/display/BannerImage.tsx
@@ -0,0 +1,22 @@
+import { mdiImage } from "@mdi/js";
+import Icon from "@mdi/react";
+
+type Props = {
+    className?: string;
+  image?: string | null;
+  name: string;
+};
+export const BannerImage: React.FC<Props> = ({ className, image, name }) => {
+  return (
+    <div className={` bg-slate-200 flex items-center justify-center w-full h-40 ${className}`}>
+      {image ? (
+        <img className="w-full h-full object-cover object-center" src={image} alt={name} />
+      ) : (
+        <div className='flex flex-col items-center opacity-30'>
+            <Icon path={mdiImage} size={2} />
+            <p className="text-sm font-semibold">No Image</p>
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ ui/src/components/display/IconImage.tsx
@@ -0,0 +1,23 @@
+import { mdiImage } from "@mdi/js";
+import Icon from "@mdi/react";
+
+type Props = {
+  className?: string;
+  image?: string | null;
+  name: string;
+  rounded?: boolean;
+};
+export const IconImage: React.FC<Props> = ({ className, image, name, rounded = true }) => {
+  return (
+    <div className={`bg-slate-200 flex items-center justify-center size-40 ${rounded ? 'rounded-full' : 'rounded-sm'} overflow-hidden shadow-md ${className}`}>
+      {image ? (
+        <img className="w-full h-full object-cover object-center" src={image} alt={name} />
+      ) : (
+        <div className='flex flex-col items-center opacity-30'>
+            <Icon path={mdiImage} size={2} />
+            <p className="text-sm font-semibold">No Image</p>
+        </div>
+      )}
+    </div>
+  );
+};
