99eff5617fa39d029f266797aacd96a023bdc8a4
Brian Moore <brianmoore0213@gmail.com>
2024-11-23 18:27:38 -0500

    fe get team implementation

---
+++ ui/src/components/list/TeamList.tsx
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import {
+  GetTeamBody,
+  GetTeamQuery,
+  GetTeamResponse
+} from '../../../../api/src/types/api';
+import { Loader } from '../../components/core';
+import { HTTP_METHOD } from '../../constants';
+import useApi from '../../hooks/useApi';
+
+type Props = {
+    storeId: string;
+}
+
+export const TeamList: React.FC<Props> = ({ storeId }) => {
+  const navigate = useNavigate();
+
+  const { error, isLoading, response } = useApi<GetTeamBody, GetTeamQuery, GetTeamResponse>({
+    url: `/admin/store/${storeId}/team/list`,
+    method: HTTP_METHOD.GET,
+    params: { page: '1' },
+  });
+
+  useEffect(() => {
+    if (error) navigate(`/500?error=${error}`);
+  }, [error]);
+
+  if (isLoading) return <Loader />;
+
+  const team = response?.team;
+
+  return (
+    <>
+        {team?.map((member) => (
+          <RouterLink
+            className="flex gap-4 p-4 items-center bg-white hover:bg-slate-100 text-slate-800 border-[1px] rounded shadow-md"
+            key={member.id}
+            to={`/admin/user/${member.id}`}
+            title={`View user: ${member.name}`}
+          >
+            <p className="flex-1 whitespace-nowrap text-ellipsis overflow-hidden">{member.name}</p>
+            <p className="text-sm opacity-60 whitespace-nowrap text-ellipsis overflow-hidden">{member.email}</p>
+          </RouterLink>
+        ))}
+    </>
+  );
+};
--- ui/src/pages/private/Store.tsx
+++ ui/src/pages/private/Store.tsx
@@ -1,18 +1,23 @@
-import { useContext, useEffect } from "react";
-import { Outlet, useNavigate, useParams } from "react-router-dom";
-import { GetStorePrivateBody, GetStorePrivateQuery, GetStorePrivateResponse } from "../../../../api/src/types/api";
-import { Card, Container, Page } from "../../components/container";
-import { Loader } from "../../components/core";
-import { BannerImage, IconImage, IsPublished } from "../../components/display";
-import { H1 } from "../../components/typography";
-import { HTTP_METHOD } from "../../constants";
-import { ModalContext } from "../../context/ModalContext";
-import useApi from "../../hooks/useApi";
+import { mdiDelete, mdiPencil, mdiPlus } from '@mdi/js';
+import Icon from '@mdi/react';
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { GetStorePrivateBody, GetStorePrivateQuery, GetStorePrivateResponse } from '../../../../api/src/types/api';
+import { Card, Container, Page } from '../../components/container';
+import { Loader } from '../../components/core';
+import { BannerImage, IconImage, IsPublished } from '../../components/display';
+import { Button } from '../../components/interactive';
+import { ProductList, TeamList } from '../../components/list';
+import { H1 } from '../../components/typography';
+import { HTTP_METHOD } from '../../constants';
+import { ModalContext } from '../../context/ModalContext';
+import useApi from '../../hooks/useApi';
 
 export const StoreLayout: React.FC = () => {
   const { setModal } = useContext(ModalContext);
   const { storeId } = useParams();
   const navigate = useNavigate();
+  const [activeList, setActiveList] = useState<'products' | 'team'>('products');
 
   const { error, isLoading, response } = useApi<GetStorePrivateBody, GetStorePrivateQuery, GetStorePrivateResponse>({
     url: `/admin/store/${storeId}`,
@@ -22,6 +27,10 @@ export const StoreLayout: React.FC = () => {
   useEffect(() => {
     if (error) navigate(`/500?error=${error}`);
   }, [error]);
+
+  const toggleList = (list: 'products' | 'team') => {
+    setActiveList(list);
+  };
 
   if (isLoading) return <Loader />;
 
@@ -34,17 +43,39 @@ export const StoreLayout: React.FC = () => {
           <div className="relative h-40">
             <BannerImage className="absolute top-0 left-0" image={store?.bannerImage} name={store!.name} />
             <IconImage className="absolute -bottom-12 left-8 z-10" image={store?.image} name={store!.name} />
-            
-            <div className='absolute bottom-4 right-0 px-4 pl-44 flex gap-4 items-end justify-between w-full z-10 !text-white'>
-              <H1 className='whitespace-nowrap text-ellipsis overflow-hidden text-shadow'>{store!.name}</H1>
+            <div className="absolute top-4 right-4 flex gap-4 z-10">
+              <Button variant="destructive">
+                <Icon path={mdiDelete} size={0.75} />
+              </Button>
+              <Button>
+                <Icon path={mdiPencil} size={0.75} />
+              </Button>
+            </div>
+            <div className="absolute bottom-4 right-0 px-4 pl-44 flex gap-4 items-end justify-between w-full z-10 !text-white">
+              <H1 className="whitespace-nowrap text-ellipsis overflow-hidden text-shadow">{store!.name}</H1>
               <IsPublished isPublished={store!.isPublished} pathType="store" longForm invert />
             </div>
           </div>
-          <div className='h-12'>
-
+          <div className="flex gap-4 p-4 pt-0 pl-44 items-center justify-between">
+            <div className="flex items-center gap-4">
+              <button
+                onClick={() => toggleList('products')}
+                className={`${activeList === 'products' ? 'font-semibold text-sky-600' : 'opacity-50'}`}
+              >
+                View Products
+              </button>{' '}
+              |
+              <button onClick={() => toggleList('team')} className={`${activeList === 'team' ? 'font-semibold text-sky-600' : 'opacity-50'}`}>
+                View Team
+              </button>
+            </div>
+            <Button>
+              <Icon path={mdiPlus} size={1} />
+            </Button>
           </div>
         </Card>
-        <Outlet />
+        {activeList === 'products' && <ProductList storeId={storeId!} />}
+        {activeList === 'team' && <TeamList storeId={storeId!} />}
       </Container>
     </Page>
   );
