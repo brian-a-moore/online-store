98e3d5e92fcfe5bf7a928e3949957d2804e5c63c
Brian Moore <brianmoore0213@gmail.com>
2024-11-25 13:30:46 -0500

    add all users to each store in the mocks

--- mocks/src/generators/userStoreRelation.ts
+++ mocks/src/generators/userStoreRelation.ts
@@ -1,16 +1,18 @@
 import { Prisma } from '../../../api/node_modules/@prisma/client/default';
 import { db } from '../../../api/src/config/db';
 
-export const generateUserStoreRelations = async (id: string, storeIds: string[]) => {
+export const generateUserStoreRelations = async (ids: string[], storeIds: string[]) => {
   console.log('Creating user-store relations...');
 
   const relations: Prisma.UserStoreRelationUncheckedCreateInput[] = [];
 
   storeIds.forEach((storeId) => {
-    relations.push({
-      userId: id,
-      storeId,
-      roleId: 1,
+    ids.forEach((id, index) => {
+      relations.push({
+        userId: id,
+        storeId,
+        roleId: index === 0 ? 1 : 2,
+      });
     });
   });
 
