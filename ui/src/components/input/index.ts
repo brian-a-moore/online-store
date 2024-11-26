4a9cbd714a44b723f2ae80f745d75127f4296631
Brian Moore <brianmoore0213@gmail.com>
2024-11-26 16:45:00 -0500

    initial imageuploader form component

--- ui/src/components/input/index.ts
+++ ui/src/components/input/index.ts
@@ -1,9 +1,10 @@
 import CheckboxInput from './CheckboxInput';
 import ErrorText from './ErrorText';
+import ImageUploader from './ImageUpload';
 import Label from './Label';
 import SearchBox from './SearchBox';
 import SelectInput from './SelectInput';
 import SwitchInput from './SwitchInput';
 import TextInput from './TextInput';
 
-export { CheckboxInput, ErrorText, Label, SearchBox, SelectInput, SwitchInput, TextInput };
+export { CheckboxInput, ErrorText, ImageUploader, Label, SearchBox, SelectInput, SwitchInput, TextInput };
