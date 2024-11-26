3722901e8c5687dca67bae17f10c91d3ec1b97e2
Brian Moore <brianmoore0213@gmail.com>
2024-11-26 16:45:35 -0500

    made modal work with overflowing content

--- ui/src/components/core/Modal.tsx
+++ ui/src/components/core/Modal.tsx
@@ -19,19 +19,19 @@ export const Modal: React.FC<Props> = ({ modal }) => {
   return (
     <div
       className={`
-            fixed bg-black bg-opacity-30 top-0 left-0 w-full h-full z-20 p-4 flex items-center justify-center backdrop-blur-sm
+            fixed bg-black bg-opacity-30 top-0 left-0 w-screen h-screen z-20 p-4 flex items-center justify-center backdrop-blur-sm
             ${state === 'opening' ? 'opacity-100 transition-opacity duration-200' : 'opacity-0 duration-200 delay-100'}
         `}
     >
       {!clickOutsideDisabled && <button className="absolute top-0 left-0 w-full h-full" onClick={closeModal} />}
       <div
-        className={`
-                w-full max-w-[640px]
+        className={`flex
+                w-full max-w-[640px] h-auto max-h-full overflow-hidden
                 opacity-0 -translate-y-20
                 ${state === 'closing' ? 'animate-modalClose' : 'animate-modalOpen'}
             `}
       >
-        <Card>{modal.content}</Card>
+        <Card className='flex-1 overflow-hidden'>{modal.content}</Card>
       </div>
     </div>
   );
