ba2e7d26e512eb6a1185bba014d849b38d2cd744
Brian Moore <brianmoore0213@gmail.com>
2024-11-26 09:04:44 -0500

    product form first pass

--- ui/src/components/form/Product.tsx
+++ ui/src/components/form/Product.tsx
@@ -1,44 +1,81 @@
-import { useEffect } from 'react';
+import { mdiDelete } from '@mdi/js';
+import Icon from '@mdi/react';
+import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
-  GetProductDashboardBody,
-  GetProductDashboardParams,
-  GetProductDashboardResponse,
+  GetProductAdminBody,
+  GetProductAdminQuery,
+  GetProductAdminResponse,
 } from '../../../../api/src/types/api';
 import { HTTP_METHOD } from '../../constants';
+import { ModalContext } from '../../context/ModalContext';
 import useApi from '../../hooks/useApi';
 import { Loader } from '../core';
+import { Button } from '../interactive';
+import { H3 } from '../typography';
 
 type Props = {
-  storeId?: string;
-  productId?: string;
+  productId: string;
 };
 
-export const ProductForm: React.FC<Props> = ({ storeId, productId }) => {
+export const ProductForm: React.FC<Props> = ({ productId }) => {
+  const { openModal, closeModal } = useContext(ModalContext);
   const navigate = useNavigate();
 
   const { error, isLoading, response } = useApi<
-    GetProductDashboardBody,
-    GetProductDashboardParams,
-    GetProductDashboardResponse
+    GetProductAdminBody,
+    GetProductAdminQuery,
+    GetProductAdminResponse
   >(
     {
       method: HTTP_METHOD.GET,
-      url: `/dashboard/product/${productId}`,
+      url: `/admin/product/${productId}`,
     },
-    { isAutoTriggered: !!storeId && !!productId },
+    { isAutoTriggered: !!productId },
   );
 
   useEffect(() => {
     if (error) navigate(`/500?error=${error}`);
   }, [error]);
 
+  const openDeleteProductDialog = (id: string) => {
+    openModal(
+      <>
+        <H3>Delete Product</H3>
+        <p>Are you sure you want to delete this product?</p>
+        <div className="flex justify-between">
+          <Button variant="secondary" onClick={closeModal}>
+            Cancel
+          </Button>
+          <Button variant="destructive" onClick={closeModal}>
+            Delete Product
+          </Button>
+        </div>
+      </>,
+    );
+  };
+
   if (isLoading) return <Loader />;
 
   return (
-    <form>
-      <h1>Product Form</h1>
+    <form className="flex flex-col gap-4">
+      <div className="flex justify-between">
+        <H3>Edit Product</H3>
+        {productId ? (
+          <div className="flex gap-4">
+            <Button variant="secondary" title="Delete Product" onClick={() => openDeleteProductDialog(productId)}>
+              <Icon path={mdiDelete} size={0.75} />
+            </Button>
+          </div>
+        ) : null}
+      </div>
       <p>{JSON.stringify(response?.product)}</p>
+      <div className="flex justify-between">
+        <Button variant="secondary" onClick={closeModal}>
+          Cancel
+        </Button>
+        <Button onClick={closeModal}>Update Product</Button>
+      </div>
     </form>
   );
 };
--- ui/src/pages/admin/Products.tsx
+++ ui/src/pages/admin/Products.tsx
@@ -1,12 +1,10 @@
-import { mdiDelete } from '@mdi/js';
-import Icon from '@mdi/react';
 import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ListProductsAdminBody, ListProductsAdminQuery, ListProductsAdminResponse } from '../../../../api/src/types/api';
 import { Card, ColumnConfig, Container, Page, Table } from '../../components/container';
 import { Loader } from '../../components/core';
-import { Button } from '../../components/interactive';
-import { EmptyText, H3, H4 } from '../../components/typography';
+import { ProductForm } from '../../components/form';
+import { EmptyText, H4 } from '../../components/typography';
 import { HTTP_METHOD } from '../../constants';
 import { ModalContext } from '../../context/ModalContext';
 import useApi from '../../hooks/useApi';
@@ -47,35 +45,7 @@ export const ProductsAdmin: React.FC = () => {
     if (error) navigate(`/500?error=${error}`);
   }, [error]);
 
-  const openEditProductForm = (id: string) => {
-    openModal(<>
-      <div className='flex justify-between'>
-        <H3>Edit Product</H3>
-        <p>form</p>
-        <div className='flex gap-4'>
-          <Button variant='secondary' title='Delete Product' onClick={() => openDeleteProductDialog(id)}>
-            <Icon path={mdiDelete} size={0.75} />
-          </Button>
-        </div>
-      </div>
-      <p>form</p>
-      <div className='flex justify-between'>
-        <Button variant='secondary' onClick={closeModal}>Cancel</Button>
-        <Button onClick={closeModal}>Update Product</Button>
-      </div>
-    </>);
-  };
-
-  const openDeleteProductDialog = (id: string) => {
-    openModal(<>
-      <H3>Delete Product</H3>
-      <p>Are you sure you want to delete this product?</p>
-      <div className='flex justify-between'>
-        <Button variant='secondary' onClick={closeModal}>Cancel</Button>
-        <Button variant='destructive' onClick={closeModal}>Delete Product</Button>
-      </div>
-    </>);
-  };
+  const openEditProductForm = (id: string) => openModal(<ProductForm productId={id} />);
 
   if (isLoading) return <Loader />;
 
