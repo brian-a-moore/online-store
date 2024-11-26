e3618b341afbf6b50058dca0b5c632a4bb5c3a34
Brian Moore <brianmoore0213@gmail.com>
2024-11-26 11:56:19 -0500

    add alert component

---
+++ ui/src/components/container/Alert.tsx
@@ -0,0 +1,44 @@
+import { mdiAlertCircle, mdiCheckCircle, mdiInformation } from '@mdi/js';
+import Icon from '@mdi/react';
+
+const ICON = {
+  error: mdiAlertCircle,
+  warn: mdiInformation,
+  success: mdiCheckCircle,
+};
+
+const COLORS = {
+  error: {
+    background: 'bg-red-200',
+    text: 'text-red-800',
+    icon: '#991B1B',
+  },
+  warn: {
+    background: 'bg-yellow-200',
+    text: 'text-yellow-800',
+    icon: '#854D0E',
+  },
+  success: {
+    background: 'bg-green-200',
+    text: 'text-green-800',
+    icon: '#166534',
+  },
+};
+
+type Props = {
+  type: 'error' | 'warn' | 'success';
+  children: React.ReactNode;
+};
+
+export const Alert: React.FC<Props> = ({ type, children }) => {
+  return (
+    <div className={`flex ${COLORS[type].background} rounded border-[1px] border-[${COLORS[type].text}]`}>
+      <div className="bg-[rgba(100,100,100,0.1)] flex items-center justify-center px-4">
+        <Icon path={ICON[type]} size={1.25} color={COLORS[type].icon} />
+      </div>
+      <div className="flex flex-col flex-1 gap-2 p-4">
+        <p className={`${COLORS[type].text}text-left line-clamp-3 text-sm`}>{children}</p>
+      </div>
+    </div>
+  );
+};
