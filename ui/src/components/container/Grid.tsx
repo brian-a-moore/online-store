d53580be21ef26da627b407bcfd7c0223d047d3f
Brian Moore <brianmoore0213@gmail.com>
2024-11-26 09:08:00 -0500

    reduced bottom padding from 50vh to 30vh

--- ui/src/components/container/Grid.tsx
+++ ui/src/components/container/Grid.tsx
@@ -5,7 +5,7 @@ type Props = {
 export const Grid: React.FC<Props> = ({ className, children }) => {
   return (
     <div
-      className={`grid gap-4 p-4 mx-auto w-full max-w-[1280px] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 pb-[50vh] ${className}`}
+      className={`grid gap-4 p-4 mx-auto w-full max-w-[1280px] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 pb-[30vh] ${className}`}
     >
       {children}
     </div>
