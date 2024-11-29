000212a4ebfcebf6d86b950017da513d4979ab11
Brian Moore <brianmoore0213@gmail.com>
2024-11-29 03:46:04 -0500

    finally styled the 404 and 500 pages

--- ui/src/pages/public/404.tsx
+++ ui/src/pages/public/404.tsx
@@ -1,15 +1,16 @@
 import { Separator } from '../../components/display';
 import { Link } from '../../components/interactive';
+import { H3 } from '../../components/typography';
 
-type Props = {};
-
-export const PageNotFound: React.FC<Props> = () => {
+export const PageNotFound: React.FC = () => {
   return (
-    <div>
-      <h1>It seems we've gotten lost...</h1>
-      <p>The page you requested is either no longer available or doesn't exist.</p>
-      <Separator />
-      <Link href="/">Go Home</Link>
+    <div className='fixed top-0 left-0 w-screen h-screen p-8 flex items-center justify-center'>
+      <div className='flex flex-col gap-4 max-w-96'>
+        <H3>It seems we've gotten lost...</H3>
+        <p>The page you requested is either no longer available or doesn't exist.</p>
+        <Separator />
+        <Link href="/">Go Home</Link>
+      </div>
     </div>
   );
 };
--- ui/src/pages/public/500.tsx
+++ ui/src/pages/public/500.tsx
@@ -1,17 +1,27 @@
-import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { Separator } from '../../components/display';
 import { Link } from '../../components/interactive';
+import { H3 } from '../../components/typography';
 
 type Props = {};
 
 export const ServerError: React.FC<Props> = () => {
-  const { error } = useParams<{ error: string }>();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const error = queryParams.get('error');
+
   return (
-    <div>
-      <h1>Something didn't quite go right...</h1>
-      <p>{error}</p>
-      <Separator />
-      <Link href="/">Go Home</Link>
+    <div className="fixed top-0 left-0 w-screen h-screen p-8 flex items-center justify-center">
+      <div className="flex flex-col gap-4 max-w-96">
+        <H3>Something didn't quite go right...</H3>
+        <p className="text-sm font-semibold text-sky-600 -mb-2">Error Message:</p>
+        <p className="bg-sky-200 text-sky-600 px-4 py-2 rounded line-clamp-4" title={error || 'Unknown Error'}>
+          {error}
+        </p>
+        <p>If you keep seeing messages like this, please contact the site administrator.</p>
+        <Separator />
+        <Link href="/">Go Home</Link>
+      </div>
     </div>
   );
 };
