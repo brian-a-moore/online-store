d729326269d3b98f8ebfc9857674b6e171fe08e2
Brian Moore <brianmoore0213@gmail.com>
2024-11-30 20:35:54 -0500

    add size limits to image uploader

--- ui/src/components/input/ImageUploader.tsx
+++ ui/src/components/input/ImageUploader.tsx
@@ -1,9 +1,11 @@
 import { mdiDelete, mdiImageSearch, mdiResize, mdiRotateRight } from '@mdi/js';
 import Icon from '@mdi/react';
+import { SIZE } from '@sunami/constants';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import AvatarEditor from 'react-avatar-editor';
 import { useDropzone } from 'react-dropzone';
 import { Button } from '../interactive';
+import { ErrorText } from '../typography';
 
 type Props = {
   existingImage?: string;
@@ -13,22 +15,23 @@ type Props = {
 };
 
 const sizeConfigs = {
-    image: {
-        componentSize: 'w-[270px]',
-        width: 250,
-        height: 250,
-    },
-    bannerImage: {
-        componentSize: 'w-[670px]',
-        width: 650,
-        height: 100,
-    }
-}
+  image: {
+    componentSize: 'w-[270px]',
+    width: 250,
+    height: 250,
+  },
+  bannerImage: {
+    componentSize: 'w-[670px]',
+    width: 650,
+    height: 100,
+  },
+};
 
 export const ImageUploader: React.FC<Props> = ({ existingImage, config = 'image', rounded = false, onUpload }) => {
   const [image, setImage] = useState<string | File | null>(null);
   const [scale, setScale] = useState(1);
   const [roatation, setRotation] = useState(0);
+  const [error, setError] = useState<string | null>(null);
   const ref = useRef();
 
   useEffect(() => {
@@ -45,10 +48,15 @@ export const ImageUploader: React.FC<Props> = ({ existingImage, config = 'image'
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
+    if (file && file?.size > SIZE.MB * 2.5) {
+      setError('File size should be less than 2.5MB');
+      return;
+    }
     if (file) {
       setImage(file);
       setScale(1);
       setRotation(0);
+      setError(null);
     }
   };
 
@@ -64,24 +72,25 @@ export const ImageUploader: React.FC<Props> = ({ existingImage, config = 'image'
     setImage(null);
     setScale(1);
     setRotation(0);
+    setError(null);
   };
 
   const onSave = () => {
-    if(ref.current) {
+    if (ref.current) {
       // @ts-ignore
       const canvas = ref.current.getImageScaledToCanvas();
       canvas.toBlob((blob: Blob | null) => {
-        if(blob) {
+        if (blob) {
           const file = new File([blob], 'avatar.png', { type: 'image/png' });
           onUpload(file);
         }
       });
-    };
+    }
   };
 
   return (
     <div className={`flex flex-col gap-4 items-center ${sizeConfigs[config].componentSize}`} {...getRootProps()}>
-      <div className='bg-slate-300'>
+      <div className="bg-slate-300">
         <AvatarEditor
           backgroundColor="#CBD5E1"
           width={sizeConfigs[config].width}
@@ -113,16 +122,21 @@ export const ImageUploader: React.FC<Props> = ({ existingImage, config = 'image'
           {...getInputProps}
         />
       </div>
-      <div className='flex w-full justify-center gap-4'>
-        <Button variant='secondary' className='flex relative gap-4 text-sm'>
+      <div className="flex w-full justify-center gap-4">
+        <Button variant="secondary" className="flex relative gap-4 text-sm">
           <Icon path={mdiImageSearch} size={0.75} color="#475569" />
-      <input type="file" onChange={handleFileChange} 
-            className="absolute inset-0 w-full h-full opacity-0 cursor-pointer" />
-      </Button>
-      <Button disabled={!image} variant='destructive' className='text-sm' onClick={onClear}>
-        <Icon path={mdiDelete} size={0.75} />
-      </Button>
+          <input
+            type="file"
+            onChange={handleFileChange}
+            accept="image/*"
+            className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+          />
+        </Button>
+        <Button disabled={!image} variant="destructive" className="text-sm" onClick={onClear}>
+          <Icon path={mdiDelete} size={0.75} />
+        </Button>
       </div>
+      {error && <ErrorText>{error}</ErrorText>}
     </div>
   );
 };
